d811543585c3e6782bb8b4995e0fc2cb5737a34d
Jordan Munch O'Hare <git@jordan.munchohare.com>
2025-07-26 18:48:43 +0200

    autoformatting

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -2,16 +2,17 @@ module Main exposing (main)
 
 import Browser exposing (element)
 import Browser.Events exposing (onKeyDown)
-import Html exposing (Html, button, div, text, img)
+import Dict exposing (Dict)
+import Html exposing (Html, button, div, img, text)
 import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
 import Json.Decode as Decode
-import Dict exposing (Dict)
 
 
 type Msg
     = Increment
     | KeyPress String
+
 
 type alias Model =
     { count : Int
@@ -21,6 +22,7 @@ type alias Model =
     , images : Dict String ImageWithMetadata
     }
 
+
 type alias ImageWithMetadata =
     { id : String
     , url : String
@@ -28,35 +30,46 @@ type alias ImageWithMetadata =
     , inAlbumns : List String
     }
 
-type alias Album = 
+
+type alias Album =
     { id : String
     , name : String
     }
 
+
 init : () -> ( Model, Cmd Msg )
-init _ = ({
-            count = 7
-            , key = ""
-            , currentImage = "0001"
-            , albums = [ { id = "a", name = "Album A" }
-                       , { id = "b", name = "Album B" } 
-                       , { id = "c", name = "Album C" }
-                       , { id = "d", name = "Album D" }
-                       , { id = "e", name = "Album E" }
-                       ]
-            , images = Dict.fromList (List.map (\image -> (image.id, image)) [ { id = "0001", url = "http://localhost:3333/images/imafight.jpg", title = "Image A", inAlbumns = ["a"] }
-                       , { id = "0002", url = "http://localhost:3333/images/dcemployees.jpg", title = "Image B", inAlbumns = ["b"] }
-                       , { id = "0003", url = "http://localhost:3333/images/jordan.jpg", title = "Image C", inAlbumns = ["a", "b"] } ] )
-            }
-        , Cmd.none
-        )
-    
+init _ =
+    ( { count = 7
+      , key = ""
+      , currentImage = "0001"
+      , albums =
+            [ { id = "a", name = "Album A" }
+            , { id = "b", name = "Album B" }
+            , { id = "c", name = "Album C" }
+            , { id = "d", name = "Album D" }
+            , { id = "e", name = "Album E" }
+            ]
+      , images =
+            Dict.fromList
+                (List.map (\image -> ( image.id, image ))
+                    [ { id = "0001", url = "http://localhost:3333/images/imafight.jpg", title = "Image A", inAlbumns = [ "a" ] }
+                    , { id = "0002", url = "http://localhost:3333/images/dcemployees.jpg", title = "Image B", inAlbumns = [ "b" ] }
+                    , { id = "0003", url = "http://localhost:3333/images/jordan.jpg", title = "Image C", inAlbumns = [ "a", "b" ] }
+                    ]
+                )
+      }
+    , Cmd.none
+    )
+
 
 showAlbumsForImage : List Album -> ImageWithMetadata -> Html msg
 showAlbumsForImage albums image =
     let
-        inAlbumns = List.filter (\album -> List.member album.id image.inAlbumns) albums
-        notInAlbumns = List.filter (\album -> not (List.member album.id image.inAlbumns)) albums
+        inAlbumns =
+            List.filter (\album -> List.member album.id image.inAlbumns) albums
+
+        notInAlbumns =
+            List.filter (\album -> not (List.member album.id image.inAlbumns)) albums
     in
     div []
         [ div []
@@ -70,13 +83,16 @@ imageOrBlank : String -> Html msg
 imageOrBlank key =
     if key == "" then
         text ""
+
     else
         img [ src key, class "img-fluid" ] []
-        
+
+
 showCurrentImage : Model -> Html msg
 showCurrentImage model =
     let
-        maybeCurrentImage = Dict.get model.currentImage model.images
+        maybeCurrentImage =
+            Dict.get model.currentImage model.images
     in
     case maybeCurrentImage of
         Just currentImage ->
@@ -88,13 +104,24 @@ showCurrentImage model =
 
         Nothing ->
             div [] [ text "No image found" ]
-            
+
+
 moveImagePointer : Model -> Int -> Model
 moveImagePointer model step =
     let
-        imageValues = (model.images |> Dict.values)
+        imageValues =
+            model.images |> Dict.values
+
         currentIndex =
-            List.indexedMap (\index image -> if image.id == model.currentImage then Just index else Nothing) imageValues
+            List.indexedMap
+                (\index image ->
+                    if image.id == model.currentImage then
+                        Just index
+
+                    else
+                        Nothing
+                )
+                imageValues
                 |> List.filterMap identity
                 |> List.head
                 |> Maybe.withDefault 0
@@ -135,10 +162,11 @@ update msg model =
                 "ArrowRight" ->
                     ( moveImagePointer model 1, Cmd.none )
 
-                " "->
-                    ( { model | key = ""}, Cmd.none )
+                " " ->
+                    ( { model | key = "" }, Cmd.none )
+
                 _ ->
-                    ( { model | key = key}, Cmd.none )
+                    ( { model | key = key }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
